f0ae7c76a0267599123397fbd7604910a5fa7fd2
CodemyCoder <94533412+CodemyCoder@users.noreply.github.com>
2022-09-09 00:21:42 +0530

    Update send_mail_with_html_content.py

--- send_mail_with_html_content.py
+++ send_mail_with_html_content.py
@@ -7,13 +7,13 @@ import smtplib
 # Do remember to create environment variables for your EmailId and Password
 User = os.getenv("Gmail_User") # Fetched from environment variables 
 Pass = os.getenv("Gmail_Pass") # Fetched from environment variables 
-dataFile = open("project.html", "r").read() # Can be any html file you created but not with advanced logic or methods.
+# dataFile = open("./project.html", "r").read() # Can be any html file you created but not with advanced logic or methods.
 msg = EmailMessage()
 msg["From"] = User
 msg["To"] = User # Here it can have all the email-id list whom you want to send the mail.
 msg["Subject"] = "Python Mail App Test Port 465" # Subject for your mail
 # msg.set_content("<h1>lorem is a dummy para by VsCode Part 2</h1>", subtype="html") # This Is used to set the mail body ( can be html or normal text nothing matters here) but it's subtype should be html
-fileData = open("./project.html", "r").read()
+fileData = open("./project.html", "r").read() # Can be any html file you created but not with advanced logic or methods.
 # msg.add_header("Content-Type", "text/html") # used to set the type and subtype of mail body when using set_payload() method
 # msg.set_payload(fileData) # another method like set_content()
 msg.set_content(fileData, subtype="html") # created mail body content by reading data from a HTML file.
